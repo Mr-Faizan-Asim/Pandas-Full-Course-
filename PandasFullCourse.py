4441813abbb8b4fb4f551c8ad5392ac18ec0ba0c
Muhammad Faizan Asim <120254196+Mr-Faizan-Asim@users.noreply.github.com>
2024-03-22 11:16:48 +0500

    3rd Till DataFrame

--- PandasFullCourse.py
+++ PandasFullCourse.py
@@ -1,8 +1,22 @@
 import pandas as pd
 print(pd.__version__)
-# Second
+# Second- Series Are Like Column
 data = [1,12,114,141]
 result  = pd.Series(data)
 print(result)
-# Work with Objects
-
+# Work with Dictionary
+data = {"Ak": 1,"Ck": 44,"Tk": 11}
+result  = pd.Series(data)
+print(result)
+# Label
+data = [1,12,114,141]
+result  = pd.Series(data,["x","y","A","a"])
+print(result)
+# Data Set
+data = {"Marks": [1,2,45,65],"Name": ["Ali","Hassan","Akram","Faizan"]}
+result  = pd.Series(data)
+print(result)
+#3rd - Dataframe
+data = {"Marks": [1,2,45,65],"Name": ["Ali","Hassan","Akram","Faizan"]}
+result  = pd.DataFrame(data)
+print(result)
